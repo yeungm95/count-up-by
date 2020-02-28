663acb090216054c30a4a8178ded96ea5fd35ade
Student <example@example.com>
2020-02-27 20:43:34 -0800

    add alert if countBy > countTo

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
     var countTo = parseInt($("#countToNumber").val())
     // console.log(countBy, countTo)
 
+    if (countBy > countTo) {  
+      alert("Hey, your count to number can't be less than your count by")
+    } else if {
+
+    }
+
     var total = 0
     for (var currentNumber = countBy; currentNumber <= countTo; currentNumber += countBy) {
       total += currentNumber
